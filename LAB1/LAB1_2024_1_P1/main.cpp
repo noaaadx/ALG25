9d5e722d51273b51cefd089eb0de6d5d9d4bda2c
Sebastian Noa <sebastiannoacjt@gmail.com>
2025-04-04 07:40:19 -0500

    nuevos cambios

---
+++ LAB1/LAB1_2024_1_P1/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: sebas
+ *
+ * Created on 3 de abril de 2025, 05:41 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
